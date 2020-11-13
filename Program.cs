111cef93f843355f05fbb855863b71b8b1499106
vitor20022003 <vitorm4123@gmail.com>
2020-11-13 03:36:51 -0300

    algoritmo media concluido

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("digite a primeira nota: ");
+            double nota1 = double.Parse(Console.ReadLine());
+
+            Console.Write("digite a segunda nota: ");
+            double nota2 = double.Parse(Console.ReadLine());
+
+            double media = (nota1 + nota2) / 2;
+
+            string situacao = "";
+
+            Console.WriteLine("media: " + media);
+
+            if(media >  7) {
+                situacao = "aprovado";
+            }
+            else{
+                situacao = "reprovado";
+            }
+
+            Console.WriteLine(situacao);
+            
+        }
+    }
+}
